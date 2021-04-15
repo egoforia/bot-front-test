79418c84480e9e7257ba6c9b68bb33289b784160
Diego Haas Sanches <egoforia@gmail.com>
2021-04-15 11:40:34 -0300

    adicionado lista de compras do revendedor

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { SaleItemModule } from '../components/sale-item/sale-item.module';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    SaleItemModule
   ],
   declarations: [HomePage]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Sale, SaleStatus } from '../api-interfaces/sale';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+
+  sales: Sale[] = [
+    { id: 1, price: 99.99, date: new Date('2021-04-15'), cashback: 9.99, status: SaleStatus.WAITING },
+    { id: 2, price: 99.99, date: new Date('2021-04-15'), cashback: 9.99, status: SaleStatus.DENIED },
+    { id: 3, price: 99.99, date: new Date('2021-04-15'), cashback: 9.99, status: SaleStatus.APPROVED },
+  ]
 
 }
