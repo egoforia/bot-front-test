9529ed93854ceb901a31bb2374323343b9b18f5a
Diego Haas Sanches <egoforia@gmail.com>
2021-04-15 10:28:44 -0300

    importado ion-mask para configurar máscara no ion-input

---
+++ src/app/directive/ion-mask.spec.ts
@@ -0,0 +1,8 @@
+import { IonMask } from './ion-mask';
+
+describe('IonMask', () => {
+  it('should create an instance', () => {
+    // const directive = new IonMask();
+    // expect(directive).toBeTruthy();
+  });
+});
