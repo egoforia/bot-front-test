21ad4f59fc9b34a204908f95c429230c63f943e4
Diego Haas Sanches <egoforia@gmail.com>
2021-04-15 11:40:23 -0300

    criado component de item da compra para exibir na lista na home page

---
+++ src/app/components/sale-item/sale-item.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Sale, SaleStatus } from 'src/app/api-interfaces/sale';
+
+@Component({
+  selector: 'app-sale-item',
+  templateUrl: './sale-item.component.html',
+  styleUrls: ['./sale-item.component.scss'],
+})
+export class SaleItemComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() sale: Sale;
+
+  ngOnInit() {}
+
+  get status(): string {
+    return SaleStatus[ this.sale.status ];
+  }
+
+}
