bb2e9abc1bf3dca372bb5cee72f48e4901679688
Diego Haas Sanches <egoforia@gmail.com>
2021-04-15 10:01:55 -0300

    configurado formulário de cadastro de revendedor

--- src/app/sign-up/sign-up.page.ts
+++ src/app/sign-up/sign-up.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MustMatch } from '../validators/must-match.validator';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  form: FormGroup;
 
   ngOnInit() {
+    this.form = this.fb.group({
+      'name':       [ null, [ Validators.required ] ],
+      'cpf':        [ null, [ Validators.required ] ],
+      'email':      [ null, [ Validators.required, Validators.email ]],
+      'password':   [ null, [ Validators.required, Validators.minLength(8) ] ],
+      'cpassword':  [ null, [] ]
+    }, {
+      validator: MustMatch('password', 'cpassword')
+    });
   }
 
+  get name(): AbstractControl {
+    return this.form.controls.name;
+  }
+
+  get cpf(): AbstractControl {
+    return this.form.controls.cpf;
+  }
+
+  get email(): AbstractControl {
+    return this.form.controls.email;
+  }
+
+  get password(): AbstractControl {
+    return this.form.controls.password;
+  }
+
+  get cpassword(): AbstractControl {
+    return this.form.controls.cpassword;
+  }
+
+  goToLoginPage() {
+    
+  }
+
+  submit() {
+    if (this.form.valid) {
+
+    }
+  }
 }
