0e845bd99ca1d700443ee8319c66db1e7342f75e
Diego Haas Sanches <egoforia@gmail.com>
2021-04-17 16:26:45 -0300

    implementado fluxo de sign up com requisição para api fake

--- src/app/sign-up/sign-up.page.ts
+++ src/app/sign-up/sign-up.page.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoadingController, ToastController } from '@ionic/angular';
 import { MASKS, NgBrazilValidators } from 'ng-brazil';
+import { User } from '../api-interfaces/user';
+import { AuthFactory } from '../factories/auth-factory';
+import { AuthService } from '../services/auth.service';
 import { MustMatch } from '../validators/must-match.validator';
 
 @Component({
@@ -14,6 +18,9 @@ export class SignUpPage implements OnInit {
   public MASKS = MASKS;
 
   constructor(
+    private authService: AuthService,
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
     private fb: FormBuilder,
     private router: Router
   ) { }
@@ -56,9 +63,42 @@ export class SignUpPage implements OnInit {
     this.router.navigate(['login']);
   }
 
-  submit() {
+  async submit() {
     if (this.form.valid) {
+      const loading = await this.loadingCtrl.create({ message: 'Aguarde um momento' });
+      loading.present();
+      
+      const user = AuthFactory.buildUserFromForm(this.form);
 
+      // set timeout para simular demora de uma requisição real
+      setTimeout(() => { 
+        this.authService.signUp(user)
+          .then(async (user: User) => {
+            const toast = await this.toastCtrl.create({
+              message: `Bem vindo ao Boti, ${user.name}!`,
+              color: 'success',
+              duration: 2000,
+              position: 'top'
+            });
+  
+            toast.present();
+  
+            this.router.navigate(['home'], { replaceUrl: true });
+          })
+          .catch(async e => {
+            const toast = await this.toastCtrl.create({
+              message: `Ops! Algo ocorreu errado. Verifique suas informações e tente novamente.`,
+              color: 'danger',
+              duration: 2000,
+              position: 'top'
+            });
+  
+            toast.present();
+          })
+          .finally(() => {
+            loading.dismiss();
+          });
+      }, 600);
     }
   }
 }
