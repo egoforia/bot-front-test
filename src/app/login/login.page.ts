bb2753a58137eade1211e01e80cae7f64e8619fd
Diego Haas Sanches <egoforia@gmail.com>
2021-04-17 16:32:33 -0300

    implementado fluxo de login com requisição para api fake

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoadingController, ToastController } from '@ionic/angular';
+import { User } from '../api-interfaces/user';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +13,9 @@ import { Router } from '@angular/router';
 export class LoginPage implements OnInit {
 
   constructor(
+    private authService: AuthService,
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
     private fb: FormBuilder,
     private router: Router
   ) { }
@@ -35,9 +41,40 @@ export class LoginPage implements OnInit {
     this.router.navigate(['sign-up']);
   }
 
-  submit() {
+  async submit() {
     if (this.form.valid) {
-      this.router.navigate(['home'], { replaceUrl: true });
+      const loading = await this.loadingCtrl.create({ message: 'Aguarde um momento' });
+      loading.present();
+      
+      // set timeout para simular demora de uma requisição real
+      setTimeout(() => {
+        this.authService.login(this.email.value, this.password.value)
+          .then(async (user: User) => {
+            const toast = await this.toastCtrl.create({
+              message: `Bem vindo de volta, ${user.name}!`,
+              color: 'success',
+              duration: 2000,
+              position: 'top'
+            });
+  
+            toast.present();
+            this.router.navigate(['home'], { replaceUrl: true });
+          })
+          .catch(async e => {
+            // toast para simular mensagem de erro de autenticação
+            const toast = await this.toastCtrl.create({
+              message: `Email ou senha incorretos. Por favor, tente novamente.`,
+              color: 'danger',
+              duration: 2000,
+              position: 'top'
+            });
+  
+            toast.present();
+          })
+          .finally(() => {
+            loading.dismiss();
+          });
+      }, 600);
     }
   }
 
