93b8376626336ab51c092a97d757701f977fc7de
Diego Haas Sanches <egoforia@gmail.com>
2021-04-18 11:53:31 -0300

    removido import não utilizado

--- src/app/components/add-sale-modal/add-sale-modal.module.ts
+++ src/app/components/add-sale-modal/add-sale-modal.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule, DatePipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { AddSaleModalComponent } from './add-sale-modal.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
