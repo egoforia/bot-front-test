820309a632b680fd11ebe6fd0fa4d8d8e5f557aa
Diego Haas Sanches <egoforia@gmail.com>
2021-04-18 18:27:11 -0300

    removido código não utilizado de auth.actions

--- src/app/actions/auth.actions.ts
+++ src/app/actions/auth.actions.ts
@@ -1,17 +1,6 @@
 import { Action, createAction, props } from '@ngrx/store';
 import { User } from '../api-interfaces/user';
 
-export enum AuthActionTypes {
-  Login           = '[Login Page] Login',
-  LoginComplete   = '[Login Page] Login Complete',
-  LoginSuccess    = '[Auth API] Login Success',
-  LoginFailure    = '[Auth API] Login Failure',
-  CheckLogin      = '[Auth] Check Login',
-  Logout          = '[Auth] Confirm Logout',
-  LogoutCancelled = '[Auth] Logout Cancelled',
-  LogoutConfirmed = '[Auth] Logout Confirmed'
-}
-
 export const login = createAction(
   '[Auth] Login',
   props<{ email: string, password: string }>()
