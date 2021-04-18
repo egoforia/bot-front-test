120de8992d80bf09df07f54bb710f63557b8c99c
Diego Haas Sanches <egoforia@gmail.com>
2021-04-18 11:32:25 -0300

    configurado angular com locale pt-BR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -9,6 +9,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { NgBrazil } from 'ng-brazil';
 import { TextMaskModule } from 'angular2-text-mask';
 import { HttpClientModule } from '@angular/common/http';
+
+import ptBr from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(ptBr);
 
 @NgModule({
   declarations: [AppComponent],
@@ -25,6 +30,9 @@ import { HttpClientModule } from '@angular/common/http';
     { 
       provide: RouteReuseStrategy, 
       useClass: IonicRouteStrategy 
+    }, {
+      provide: LOCALE_ID, 
+      useValue: 'pt-BR'
     }
   ],
   bootstrap: [AppComponent],
