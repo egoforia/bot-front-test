2b68d9dfcc9091644d867fa1312d8f43cfc8b039
Diego Haas Sanches <egoforia@gmail.com>
2021-04-19 13:33:17 -0300

    unit test para o authService.login

--- src/app/api-interfaces/user.ts
+++ src/app/api-interfaces/user.ts
@@ -1,7 +1,7 @@
 export interface User {
   id?: number;
-  name: string;
+  name?: string;
   email: string;
-  cpf: string;
+  cpf?: string;
   password?: string;
 }
--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,10 +1,13 @@
 import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { AuthService } from './auth.service';
 
+
 describe('AuthService', () => {
   let service: AuthService;
+  let httpClientSpy: { post: jasmine.Spy };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -12,10 +15,29 @@ describe('AuthService', () => {
         HttpClientModule
       ]
     });
-    service = TestBed.inject(AuthService);
+
+    httpClientSpy = jasmine.createSpyObj('HttpClient', [ 'post' ])
+    service = new AuthService(httpClientSpy as any);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('login', () => {
+    it('should return an Observable<User>', () => {
+      const email = 'john@doe.com';
+      const password = '12341234';
+      const stubUser = { email, password };
+
+      httpClientSpy.post.and.returnValue(of(stubUser));
+
+      service.login(email, password).subscribe(
+        user => expect(user).toEqual(stubUser, 'expected user'),
+        fail
+      )
+
+      expect(httpClientSpy.post.calls.count()).toBe(1, 'one call');
+    });
+  })
 });
