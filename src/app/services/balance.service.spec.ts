fe2d80ddd4b325ea6cd46ceef8a58984d2408c74
Diego Haas Sanches <egoforia@gmail.com>
2021-04-19 13:36:41 -0300

    unit test para balanceService.getUserBalance

--- src/app/services/balance.service.spec.ts
+++ src/app/services/balance.service.spec.ts
@@ -1,10 +1,12 @@
 import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { BalanceService } from './balance.service';
 
 describe('BalanceService', () => {
   let service: BalanceService;
+  let httpClientSpy: { get: jasmine.Spy };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -12,10 +14,27 @@ describe('BalanceService', () => {
         HttpClientModule
       ]
     });
-    service = TestBed.inject(BalanceService);
+
+    httpClientSpy = jasmine.createSpyObj('HttpClient', [ 'get' ]);
+    service = new BalanceService(httpClientSpy as any);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('getUserBalance', () => {
+    it('should return an Observable<User>', () => {
+      const stubValue = { id: 1, user_id: 1, total: 999.99 };
+
+      httpClientSpy.get.and.returnValue(of(stubValue));
+
+      service.getUserBalance(1).subscribe(
+        balance => expect(balance).toEqual(stubValue, 'expected balance'),
+        fail
+      )
+
+      expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+    });
+  })
 });
